2fcb134df269bf8d9ac158acbf6c21af6d2a90dd
huimiu <miuhui@outlook.com>
2023-09-24 21:38:12 +0800

    style: prompt list layout

--- app/prompts/page.tsx
+++ app/prompts/page.tsx
@@ -10,7 +10,7 @@ export const metadata: Metadata = {
 
 export default function Prompts() {
   return (
-    <div className='grid md:gap-4 xl:gap-4 p-4 justify-center'>
+    <div className='grid md:gap-4 xl:gap-4 p-4 md:w-2/3 m-auto'>
       {prompts.map((p) => (
         <PromptCard key={p.id} prompt={p} />
       ))}
