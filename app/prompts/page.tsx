1d8c340806387b261540a29feb87e8abdb5f06e3
huimiu <miuhui@outlook.com>
2023-10-17 22:06:32 +0800

    feat: add create button for prompt list page

--- app/prompts/page.tsx
+++ app/prompts/page.tsx
@@ -1,14 +1,18 @@
 'use client';
 
+import { useRouter } from 'next/navigation';
 import { useEffect, useState } from 'react';
 
 import { PromptCard } from '@/components/prompt-card';
 import { PromptCardLoading } from '@/components/prompt-card-loading';
+import { Button } from '@/components/ui/button';
+import { Skeleton } from '@/components/ui/skeleton';
 import { Prompt } from '@/data/prompts';
 
 export default function Prompts() {
   const [prompts, setPrompts] = useState<Prompt[]>([]);
   const [loading, setLoading] = useState(true);
+  const router = useRouter();
 
   useEffect(() => {
     getPrompts()
@@ -24,11 +28,22 @@ export default function Prompts() {
     <>
       {loading ? (
         <div className='container grid gap-6 py-6'>
+          <div className='flex w-full'>
+            <Skeleton className='ml-auto h-10 w-16' />
+          </div>
           <PromptCardLoading />
           <PromptCardLoading />
         </div>
       ) : (
         <div className='container grid gap-4 py-6'>
+          <div className='flex w-full'>
+            <Button
+              className='ml-auto'
+              onClick={(e) => router.push(`/prompts/create`)}
+            >
+              Add
+            </Button>
+          </div>
           {prompts.map((p) => (
             <PromptCard key={p.id} prompt={p} />
           ))}
